8ae77759c3e0f97d3802e6ca0a7bd087945c2f0c
clubk <clubk@NightshadePC>
2018-08-07 13:26:42 -0500

    Forgot to add the file

---
+++ src/Vars/OptionArrayList.java
@@ -0,0 +1,21 @@
+package Vars;
+
+import java.util.ArrayList;
+
+public class OptionArrayList extends ArrayList<ArrayList<String>>{
+
+	String name;
+	
+	public OptionArrayList(){
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	
+}
